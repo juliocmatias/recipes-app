9ceb581bd8d5f0372e4e2a5932fcdfad1ad5856a
Julio Matias <juliocesark2@hotmail.com>
2023-12-18 23:26:03 -0300

    :books: docs: Adicionando as funcções das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
+import MealRecipe from './pages/MealRecipe';
+import Drinks from './pages/Drinks';
+import DrinkRecipe from './pages/DrinkRecipe';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipeInProgress from './pages/RecipeInProgress';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/meals" element={ <Meals /> } />
+      <Route path="/meals/:id" element={ <MealRecipe /> } />
+      <Route path="/drinks" element={ <Drinks /> } />
+      <Route path="/drinks/:id" element={ <DrinkRecipe /> } />
+      <Route path="/profile" element={ <Profile /> } />
+      <Route path="/done-recipes" element={ <DoneRecipes /> } />
+      <Route path="/favorite-recipes" element={ <FavoriteRecipes /> } />
+      <Route path="/drinks/:id/in-progress" element={ <RecipeInProgress /> } />
+      <Route path="/meals/:id/in-progress" element={ <RecipeInProgress /> } />
+    </Routes>
   );
 }
 
